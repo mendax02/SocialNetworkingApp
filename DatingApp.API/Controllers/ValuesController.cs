71a482c6208840c3265298f00b194d3fd76d1ebb
mendax02 <siddharth.rawat@outlook.com>
2019-05-19 11:26:56 +0530

    Ui Registration and login

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -17,11 +17,11 @@ namespace DatingApp.API.Controllers
         private readonly DataContext _context;
 
         public ValuesController(DataContext context)
-        {
+        { 
             _context = context;
 
         }
-        // GET api/values
+       [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
