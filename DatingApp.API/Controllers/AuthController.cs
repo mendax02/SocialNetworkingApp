4f6a90835214ab0a09df834642f6ef88b5e1c0aa
mendax02 <siddharth.rawat@outlook.com>
2019-05-19 15:36:03 +0530

    ErrorHandling on Client Side

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -47,7 +47,7 @@ namespace DatingApp.API.Controllers
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
-        {
+        {   
             var userFromRepo = await _repo.LoginAsync(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
             if (userFromRepo == null)
                 return Unauthorized();
